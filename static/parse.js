a7e2d57ca8ee8fec5be30fe41fb2afec212d869e
Jack Warnock <2243366w@student.gla.ac.uk>
2018-11-18 17:07:07 +0000

    Nearly got parsing

--- static/parse.js
+++ static/parse.js
@@ -1,25 +1,26 @@
-function parseXML(input, text) {
+function parseXML(event, input, text) {
     var readXml=null;
     event.preventDefault();
     var selectedFile = document.getElementById(input).files[0];
 	 console.log("File Selected");
-    // console.log(selectedFile);
+    console.log(selectedFile);
     var reader = new FileReader();
-    reader.onload = parseCall(e, input, text);
+    reader.onload = parseCall(readXml, event, input, text);
+	 reader.readAsText(selectedFile);
 }
 
-function parseCall(e, input, text){
-	 // console.log("in function");
+function parseCall(readXml, e, input, text){
+	 console.log("in function");
 	 readXml=e.target.result;
-	 // console.log(readXml);
+	 console.log("readXML");
+	 console.log(readXml);
 	 var parser = new DOMParser();
 	 var doc = parser.parseFromString(readXml, "application/xml");
-	 // console.log(doc);
-	 // console.log("text");
+	 console.log(doc);
+	 console.log("text");
 	 document.getElementById(text).innerHTML =
 		 doc.getElementsByTagName("name")[0].childNodes[0].nodeValue;
 
 	  L.marker([doc.getElementsByTagName("trkpt")[0].childNodes[1].nodeValue,
 		  doc.getElementsByTagName("trkpt")[0].childNodes[1].nodeValue]).addTo(map).bindPopup('Location 2').openPopup();
-}
 }
